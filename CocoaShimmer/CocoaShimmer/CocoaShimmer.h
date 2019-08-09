ec9e547e0db025b12cca757d6546243c49ce92a2
tauseef <tosifdexbytes@gmail.com>
2019-08-09 15:18:06 +0530

    added

---
+++ CocoaShimmer/CocoaShimmer/CocoaShimmer.h
@@ -0,0 +1,19 @@
+//
+//  CocoaShimmer.h
+//  CocoaShimmer
+//
+//  Created by .SY on 09/08/19.
+//  Copyright © 2019 Dexbytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaShimmer.
+FOUNDATION_EXPORT double CocoaShimmerVersionNumber;
+
+//! Project version string for CocoaShimmer.
+FOUNDATION_EXPORT const unsigned char CocoaShimmerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaShimmer/PublicHeader.h>
+
+
